01c88849e5cea31002b09dd00f80168f69f7eb5c
maathlete <46388737+maathlete@users.noreply.github.com>
2020-03-05 21:46:54 -0800

    Update multithreading_example.py

--- multithreading_example.py
+++ multithreading_example.py
@@ -5,7 +5,6 @@ Created on Thu Mar  5 20:02:14 2020
 
 @author: alexandra
 """
-
 import multiprocessing
 import time
 import numpy as np
@@ -17,7 +16,9 @@ print("Number of Cores:" + str(multiprocessing.cpu_count()))
 
 start = time.time()
 
-for i in range(0,10):
+N = 10
+
+for i in range(0,N):
     sleep(1)
     
 end  = time.time()
@@ -25,8 +26,8 @@ print("Serial execution took: " + str(end-start) + str(" seconds"))
 
 start = time.time()
 
-pool = multiprocessing.Pool(4)
-pool.map(sleep, [1 for i in range(0,10)])
+pool = multiprocessing.Pool(N)
+pool.map(sleep, [1 for i in range(0,N)])
 
 end  = time.time()
 print("Multithreading took: " + str(round(end-start,2)) + str(" seconds"))
